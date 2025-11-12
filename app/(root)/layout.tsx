c399187376b61d676a6321a3beedacb9fdc93c2b
Noorain Shaikh <noorainshaikh308@gmail.com>
2025-11-12 20:26:00 +0530

    Chaned loyout to Layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,7 @@
 import Header from "@/app/Header"
 
 
-const layout = ({children} : {children : React.ReactNode}) => {
+const Layout = ({children} : {children : React.ReactNode}) => {
   return (
     <main className="min-h-screen text-gray-400">
         <Header/>
@@ -13,4 +13,4 @@ const layout = ({children} : {children : React.ReactNode}) => {
   )
 }
 
-export default layout
+export default Layout
